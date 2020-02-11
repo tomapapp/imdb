8328e0e8cef2a5ea3098fa3aef88c9b83af68d4e
Prima Aulia Gusta <primAulia@gmail.com>
2020-02-11 16:36:49 +0100

    prep livecode

---
+++ scraper.rb
@@ -0,0 +1,5 @@
+def fetch_movies_url
+end
+
+def scrape_movie
+end
---
+++ spec/scraper_spec.rb
@@ -0,0 +1,17 @@
+# data for #fetch_movies_url:
+# [
+#   "http://www.imdb.com/title/tt0111161/",
+#   "http://www.imdb.com/title/tt0068646/",
+#   "http://www.imdb.com/title/tt0071562/",
+#   "http://www.imdb.com/title/tt0468569/",
+#   "http://www.imdb.com/title/tt0050083/"
+# ]
+
+# data for #scrape_movie:
+# {
+#   cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
+#   director: "Christopher Nolan",
+#   storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+#   title: "The Dark Knight",
+#   year: 2008
+# }
