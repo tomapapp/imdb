fcaf9c81d6a70777b79e7ccf4ffac701406ae585
Tamas Papp <toma.papp92@gmail.com>
2020-02-12 11:09:02 +0100

    IMDB scrapper

--- scraper.rb
+++ scraper.rb
@@ -5,15 +5,24 @@ def fetch_movies_url
   # scrape https://www.imdb.com/chart/top
 
   top_url = []
+  html_file = open("https://www.imdb.com/chart/top/").read
+  html_doc = Nokogiri::HTML(html_file)
+
+  html_doc.search('.titleColumn a').each do |element|
+    top_url << "http://www.imdb.com" + element.attribute('href').value
+  end
+
+
 
   # identify which class to scrape
+  # .lister-item-header
   # target the class using nokogiri method... #search
   # #search output is an array
   # take only first 5 with #first method #take
   # iterate the attribute --> href
   # push the value of href into top_url
-  
-  return top_url
+
+  return top_url.first(5)
 
 
   # output
@@ -26,8 +35,17 @@ def fetch_movies_url
   # ]
 end
 
+p fetch_movies_url
+
 def scrape_movie(url)
   # scrape individual movie url
+  movie_hash = {}
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search('.credit_summary_item h1').map do |element|
+    movie_hash[element.text.strtip] = 1
+  end
+  return movie_hash
 
   # {
   #   cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
@@ -37,3 +55,5 @@ def scrape_movie(url)
   #   year: 2008
   # }
 end
+
+p scrape_movie("http://www.imdb.com/title/tt0050083/")
